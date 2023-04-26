5f2bdfaf4fdf6d841e1c03edb97b192997d85f2a
Noushin <n.najafiragheb@gmail.com>
2023-04-26 12:01:17 +0200

    add delete redundant dictionaries

--- main.py
+++ main.py
@@ -1,18 +1,73 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import pymongo
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
 
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["Industries"]
-    collection = db["Companies"]
+def del_redundant(collection):
+    pipeline = [
+        {
+            "$group": {
+                "_id": {
+                    "field1": "$field1",
+                    "field2": "$field2",
+                    "field3": "$field3",
+                    # add more fields as needed
+                },
+                "ids": {
+                    "$push": "$_id"
+                },
+                "count": {
+                    "$sum": 1
+                }
+            }
+        },
+        {
+            "$match": {
+                "count": {
+                    "$gt": 1
+                }
+            }
+        },
+        {
+            "$sort": {
+                "count": -1
+            }
+        },
+        {
+            "$skip": 1
+        },
+        {
+            "$unwind": "$ids"
+        },
+        {
+            "$group": {
+                "_id": "$ids",
+                "count": {
+                    "$sum": 1
+                }
+            }
+        },
+        {
+            "$match": {
+                "count": {
+                    "$gt": 1
+                }
+            }
+        },
+        {
+            "$replaceRoot": {
+                "newRoot": {
+                    "_id": "$_id"
+                }
+            }
+        }
+    ]
 
-    # Find all documents with the old format of co_active_industries
+    result = collection.aggregate(pipeline)
+
+    for doc in result:
+        collection.delete_one({"_id": doc["_id"]})
+
+
+def edit_array(collection):
     query = {"co_active_industries": {"$regex": r'^\["'}}
     documents = collection.find(query)
 
@@ -22,4 +77,16 @@ if __name__ == '__main__':
         new_value = [s.strip('"') for s in old_value[1:-1].split(',')]
         collection.update_one({"_id": doc["_id"]}, {"$set": {"co_active_industries": new_value}})
         print(new_value)
+
+
+if __name__ == '__main__':
+
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["Industries"]
+    collection = db["Companies"]
+    edit_array(collection)
+    del_redundant(collection)
+
+    # Find all documents with the old format of co_active_industries
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
